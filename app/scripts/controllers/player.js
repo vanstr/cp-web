43c96d2d51f7740cdd42b2f2583f961730b67747
Ivan Mihnenok <imi@eiendomsinfo>
2014-09-27 16:56:13 +0300

    Player controller files

---
+++ app/scripts/controllers/player.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cpWebApp.controller:PlayerCtrl
+ * @description
+ * # PlayerCtrl
+ * Controller of the cpWebApp
+ */
+angular.module('cpWebApp')
+  .controller('PlayerCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
