e08f6681a52b1564511d955194b57e9bf3e091da
Ivan Mihnenok <imi@eiendomsinfo>
2014-11-02 22:07:38 +0200

    New dropbox app

--- app/scripts/controllers/signin.js
+++ app/scripts/controllers/signin.js
@@ -30,7 +30,7 @@ angular.module('cpWebApp')
 
             $scope.loginWithDropbox = function () {
                 var DROPBOX_AUTH_COMPLETE_URL = "http://localhost:8080/dropboxAuthComplete";
-                var DROPBOX_CLIENT_ID = "vw5zvh4m1fr72kw";
+                var DROPBOX_CLIENT_ID = "15m7p3devkbyc7z";
                 window.location.href = "https://www.dropbox.com/1/oauth2/authorize?client_id="+ DROPBOX_CLIENT_ID+"&response_type=code&redirect_uri="+DROPBOX_AUTH_COMPLETE_URL;
             };
 
