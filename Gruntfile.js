eb2665a07f87af4f3a5577b1e73945c6d22fbfd0
Ivan Mihnenok <imi@eiendomsinfo>
2014-11-25 20:42:52 +0200

    added /api prefix for authentication controller proxy updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -70,7 +70,7 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: '0.0.0.0',
+        hostname: 'localhost',
         livereload: 35729
       },
 	  proxies: [{
@@ -80,7 +80,7 @@ module.exports = function (grunt) {
 		rewrite: {
         // the key '^/api' is a regex for the path to be rewritten
         // the value is the context of the data service
-        '^/api': ''
+        '^/api': '/api'
     }
 	  }],
       livereload: {
