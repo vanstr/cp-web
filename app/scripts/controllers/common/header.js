d82752cac4326afc3cee317185e9d4a6ff1a0bc4
alex <avoroncovs@gmail.com>
2015-02-11 16:30:44 +0100

    Fixed redundant actions after logout

--- app/scripts/controllers/common/header.js
+++ app/scripts/controllers/common/header.js
@@ -9,18 +9,6 @@
  */
 angular.module('cpWebApp')
   .controller('HeaderCtrl', function ($location, growl, $scope, authService) {
-
-            $scope.logout = function () {
-                authService.logout(
-                    function (res) {
-                        $location.path('/');
-                        growl.info("Logged out");
-                    },
-                    function (err) {
-                        growl.error("Failed to logout");
-                    });
-            };
-
     $scope.accessRoles = authService.accessRoles;
     $scope.accessLevels = authService.accessLevels;
   });
